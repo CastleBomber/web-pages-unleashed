663dbb85ca65182e63a20e1870dc81b0ee3a3d6d
cBombs <richie.ucsb@gmail.com>
2023-03-18 13:18:43 -0700

    setup react folder frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+/**
+ *  Author: CastleBomber
+ *  Project: Mern-Tutorial
+ *  Date: March 16th, 2023
+ *
+ *  Acknowledgements: Traversy Media's Youtube MERN stack series
+ *
+ *  Tips:
+ *  Creates a React app with the contents a folder called frontend
+ *  npx create-react-app@latest frontend --template redux
+ *
+ *  To get app running, affirm correct folder,
+ *  npm run client <----use this one
+ *  npm run server
+ *  npm run dev - runs both server && client
+ *
+ *
+ *  cmd+D will change opening and closing tag simultaneously
+ *  cmd+D (+ D..) change multiple nearby instances of a name
+ *  cors-heroku error: open https://cors-anywhere.herokuapp.com/corsdemo
+ *
+ *  Watch out for:
+ *  In app, older syntax 'component', needs to be switched to 'element'
+ *  older syntax of 'Switch', need to be 'Router' surrounding 'Route'
+ *
+ *  URL variables may need ending '/' added to function properly
+ */
+
+function App() {
+  return <div className="App">Hellos</div>;
+}
+
+export default App;
