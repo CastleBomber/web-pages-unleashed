efdfe14fa86e000afb9bef040d5801f4b9703f97
cBombs <richie.ucsb@gmail.com>
2024-09-06 14:44:30 -0700

    mongodb, devling into schema of userModel and goalModel files

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,6 +15,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Please add a password"],
     },
+
     // walletAddress: {
     //   type: String,
     //   required: false,
