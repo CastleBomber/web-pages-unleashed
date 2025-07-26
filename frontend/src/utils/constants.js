fadac4d14c0d27297011ab59a2404c468246750c
CastleBomber <richie.ucsb@gmail.com>
2025-07-25 17:04:50 -0700

    updated Account 3, Franky

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -6,6 +6,6 @@ export const contractABI = abi.abi;
 // npx hardhat run scripts/deploy.js --network sepolia
 // npx hardhat run scripts/deploy.js --network holesky
 export const contractAddresses = {
-    sepolia: "0x63f0641AB382640Cde7De98e3b0759dd7D7Be448",
-    holesky: "0x7993A870C11ECd0a4C7Aab8b63ee65DD6eb1df44",
+    sepolia: "0xf75b92C0c7Ac153d2359C9C45af9a21e0B25e27B",
+    holesky: "0xA13B72F5457E97Dfd07FF365812257e36e7c9ffa",
 };
